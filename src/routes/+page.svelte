d401d1949dc6872ea73017baa86c196cba490a55
Manh Truong Nguyen <manh-truong.nguyen@gmail.com>
2023-11-06 17:47:57 -0500

    experimenting dictionary

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,35 @@
 <script lang="ts">
   let pRef: HTMLParagraphElement
   let text = ''
-  /* prettier-ignore */
-  let conversionDict = { a: '1072', c: '1089', d: '1281', e: '1077', h: '1211', i: '1110', j: '1112', k: '954', l: '1231', n: '1400', o: '959', p: '1088', q: '1382', u: '1405', v: '957', x: '1093', y: '1091' }
+
+  let conversionDict = {
+    'a': '1072',
+    'b': '98',
+    'c': '1089',
+    'd': '100',
+    'e': '1077',
+    'f': '102',
+    'g': '609',
+    'h': '104',
+    'i': '1110',
+    'j': '74',
+    'k': '107',
+    'l': '108',
+    'm': '109',
+    'n': '110',
+    'o': '1086',
+    'p': '1088',
+    'q': '113',
+    'r': '114',
+    't': '116',
+    'u': '965',
+    'v': '118',
+    'w': '969',
+    'x': '1093',
+    'y': '1091',
+    'z': '122',
+    '?': '63',
+  }
 
   const placeholder =
     "Amidst the bustling city, where the neon lights flicker and the sounds of traffic echo, there lies a hidden tranquility. In the heart of chaos, a moment of serenity can be found. It's in these quiet moments that the soul finds its peace, and the mind, its clarity. Each day brings new opportunities and challenges, weaving the tapestry of life with threads of laughter, tears, and endless possibilities. Embrace the unknown, for within its depths, the greatest adventures await."
@@ -38,7 +65,7 @@
 >
   <div class="md:w-1/2 space-y-5">
     <div class="w-full flex justify-center items-center">
-      <h1 class="font-bold text-3xl font-mono text-center">AI Detection Bypasser</h1>
+      <h1 class="font-bold text-3xl font-mono text-center">Unicode Lookalike Parser</h1>
     </div>
     <textarea
       bind:value={text}
